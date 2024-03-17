9e273e6dbd83dcf9a6b8f8a6d31b3b8a06a33814
Ismaël MOHAMED BOUH <ismaelmedbouh@outlook.fr>
2024-03-17 11:37:49 +0100

    Add Property POST API Route

--- components/PropertyAddForm.jsx
+++ components/PropertyAddForm.jsx
@@ -87,7 +87,7 @@ const PropertyAddForm = () => {
   };
 
   return (
-    <form>
+    <form action="/api/properties" method="POST" encType="multipart/form-data">
       <h2 className="text-3xl text-center font-semibold mb-6">Add Property</h2>
 
       <div className="mb-4">
@@ -481,7 +481,7 @@ const PropertyAddForm = () => {
         <input
           type="text"
           id="seller_name"
-          name="seller_info.name."
+          name="seller_info.name"
           className="border rounded w-full py-2 px-3"
           placeholder="Name"
           value={fields.seller_info.name}
@@ -535,6 +535,7 @@ const PropertyAddForm = () => {
           className="border rounded w-full py-2 px-3"
           accept="image/*"
           multiple
+          required
           onChange={handleImageChange}
         />
       </div>
