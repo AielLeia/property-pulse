df0ee14004394f47fbf4f92cb1170a4500f725e1
Ismaël MOHAMED BOUH <ismaelmedbouh@outlook.fr>
2024-03-19 17:25:26 +0100

    Bookmark API Route

---
+++ app/api/bookmarks/route.js
@@ -0,0 +1,44 @@
+import connectDb from '@/config/db.config';
+import User from '@/models/User';
+import { getSessionUser } from '@/utils/getSessionUser';
+
+export const dynamic = 'force-dynamic';
+
+export const POST = async (request) => {
+  try {
+    await connectDb();
+
+    const { propertyId } = await request.json();
+    const userSession = await getSessionUser();
+
+    if (!userSession || !userSession.userId) {
+      return new Response('User id is required', { status: 401 });
+    }
+
+    const { userId } = userSession;
+
+    const user = await User.findOne({ _id: userId });
+
+    let isBookmarked = user.bookmarks.includes(propertyId);
+    let message;
+
+    if (isBookmarked) {
+      user.bookmarks.pull(propertyId);
+      message = 'Bookmark removed successfully';
+      isBookmarked = false;
+    } else {
+      user.bookmarks.push(propertyId);
+      message = 'Bookmark added successfully';
+      isBookmarked = true;
+    }
+
+    await user.save();
+
+    return new Response(JSON.stringify({ isBookmarked, message }), {
+      status: 200,
+    });
+  } catch (err) {
+    console.error(err);
+    return new Response('Something went wrong', { status: 500 });
+  }
+};
