74a547ec117e02eee852c2e800cee25f6c349833
Ismaël MOHAMED BOUH <ismaelmedbouh@outlook.fr>
2024-03-22 18:29:46 +0100

    Delete Messages

--- app/api/messages/[id]/route.js
+++ app/api/messages/[id]/route.js
@@ -37,3 +37,38 @@ export const PUT = async (request, { params }) => {
     return new Response('Something went wrong', { status: 500 });
   }
 };
+
+export const DELETE = async (request, { params }) => {
+  try {
+    await connectDb();
+
+    const { id } = params;
+    const userSession = await getSessionUser();
+
+    if (!userSession || !userSession.userId) {
+      return new Response(
+        JSON.stringify({ message: 'You must be logged in to send a message' }),
+        { status: 401 }
+      );
+    }
+
+    const { userId } = userSession;
+
+    const message = await Message.findById(id);
+
+    if (!message) return new Response('message not found', { status: 400 });
+
+    if (message.recipient.toString() !== userId) {
+      return new Response('Unauthorized', { status: 401 });
+    }
+
+    await message.deleteOne();
+
+    return new Response(
+      JSON.stringify({ message: 'Message successfully deleted' }),
+      { status: 201 }
+    );
+  } catch (err) {
+    return new Response('Something went wrong', { status: 500 });
+  }
+};
--- components/Message.jsx
+++ components/Message.jsx
@@ -3,6 +3,7 @@ import { toast } from 'react-toastify';
 
 const Message = ({ message }) => {
   const [isRead, setIsRead] = useState(message.read);
+  const [isDeleted, setIsDeleted] = useState(false);
 
   const handleReadClick = async () => {
     try {
@@ -23,6 +24,24 @@ const Message = ({ message }) => {
       toast.error(err);
     }
   };
+
+  const handleDeleteClick = async () => {
+    try {
+      const response = await fetch(`/api/messages/${message._id}`, {
+        method: 'DELETE',
+      });
+
+      if (response.status === 201) {
+        setIsDeleted(true);
+        toast.success('Message deleted');
+      }
+    } catch (err) {
+      toast.error(err);
+      setIsDeleted(false);
+    }
+  };
+
+  if (isDeleted) return null;
 
   return (
     <div className="relative bg-white p-4 rounded-md shadow-md border border-gray-200">
@@ -65,7 +84,10 @@ const Message = ({ message }) => {
       >
         {isRead ? 'Mark as New' : 'Mark As Read'}
       </button>
-      <button className="mt-4 bg-red-500 text-white py-1 px-3 rounded-md">
+      <button
+        className="mt-4 bg-red-500 text-white py-1 px-3 rounded-md"
+        onClick={handleDeleteClick}
+      >
         Delete
       </button>
     </div>
